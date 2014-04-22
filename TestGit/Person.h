52a4ea414aba4a3d7e72ea800af4d28dd2f39466
miaogaoliang <miaogaoliang@kuark.com>
2014-04-22 18:24:15 +0800

    new File added

---
+++ TestGit/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  TestGit
+//
+//  Created by kaxiaoer on 14-4-22.
+//  Copyright (c) 2014年 miaogaoliang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ TestGit/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestGit
+//
+//  Created by kaxiaoer on 14-4-22.
+//  Copyright (c) 2014年 miaogaoliang. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
